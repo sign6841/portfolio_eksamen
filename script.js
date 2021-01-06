7d2fdd46e8b6aabfa3bff2583e5a207884c042f2
sign6841 <70881642+sign6841@users.noreply.github.com>
2021-01-06 10:24:19 +0100

    første commit

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#links").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#links").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰"
+    } else {
+        document.querySelector("#menuknap").textContent = "×"
+    }
+}
